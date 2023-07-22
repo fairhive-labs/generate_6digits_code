e328aab0dd25c0e9e7e08bb1c74d2929d578d8ac
_whyvrafvr <jsie@trendev.fr>
2023-07-22 23:27:34 +0200

    set length; open to different code length ;)

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 
-var max = 1000000;
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv))
+    .option('length', {
+        alias: 'l',
+        type: 'number',
+        description: 'length of code',
+        default: 6
+    })
+    .parse()
+
+var max = 10 ** argv.l
 var code = Math.floor(Math.random() * max).toLocaleString('en-US', {
-    minimumIntegerDigits: 6,
+    minimumIntegerDigits: argv.length,
     useGrouping: false
 })
+
 console.log('Your code:', `\x1b[33m${code} \x1b[0m`);
